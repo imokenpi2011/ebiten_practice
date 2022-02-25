b4c406161e1b1426414fee76eb9a42883193eb9e
anthem-landing18 <imokenpi2011@gmail.com>
2022-02-26 02:53:31 +0900

    ゲームの初期化、画像の取り込みメソッドの追加

--- main.go
+++ main.go
@@ -1,13 +1,50 @@
 package main
 
 import (
+	_ "embed"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"golang.org/x/image/font"
+)
+
+const (
+	screenX = 640
+	screenY = 480
+)
+
+//go:embed resources/images/dinosaur_01.png
+var byteDinosaur1Img []byte
+
+//go:embed resources/images/dinosaur_02.png
+var byteDinosaur2Img []byte
+
+//go:embed resources/images/tree_small.png
+var byteTreeSmallImg []byte
+
+//go:embed resources/images/tree_big.png
+var byteTreeBigImg []byte
+
+//go:embed resources/images/ground.png
+var byteGroundImg []byte
+
+var (
+	dinosaur1Img *ebiten.Image
+	dinosaur2Img *ebiten.Image
+	treeSmallImg *ebiten.Image
+	treeBigImg   *ebiten.Image
+	groundImg    *ebiten.Image
+	arcadeFont   font.Face
 )
 
 // ゲームのインターフェース
 type Game struct{}
+
+// ゲームの初期化を行う
+func NewGame() *Game {
+	g := &Game{}
+	return g
+}
 
 // ゲームの状態を60fpsで更新する
 func (g *Game) Update() error {
@@ -20,11 +57,6 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	// ここにゲームのレンダリングを描きます
 
 }
-
-const (
-	screenX = 640
-	screenY = 480
-)
 
 // 画面サイズを返す
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
